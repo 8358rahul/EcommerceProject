4b31ddcc2db1ea4b43e6e456af131fed800eddc3
Rahul Shanisare <85982486+8358rahul@users.noreply.github.com>
2022-02-26 18:21:22 +0530

    allow permission

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s)!$qx!s0dczf%9vbzt05##rwe5k&t!l45x%na)vu_w5uzl53a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
